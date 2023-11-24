f93c7f9128e39640b68fb876dca56872e6d0abfe
Ivan Skordev <maymunskoa@yahoo.com>
2023-11-24 17:08:31 +0200

    perform call to word api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch(process.env.REACT_APP_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
   return (
     <>
       <h1>TEST</h1>
