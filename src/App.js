cbdc6119fcb6bb31c4591fd2b8366ac7c3521d4a
Ivan Skordev <maymunskoa@yahoo.com>
2023-11-24 23:50:45 +0200

    keydown event inside handler function and unifystring utility function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import { useEffect, useState, useRef } from 'react';
-import { randomIndex as index } from './utils/helpers';
+import { randomIndex as index, unifyString } from './utils/helpers';
 import Grid from './components/Grid';
 import './App.css';
 
 function App() {
   const effectRan = useRef(false);
   const [word, setWord] = useState('');
+  const [guessWord, setGuessWord] = useState('');
+
+  useEffect(() => {
+    function keyPressHandler(event) {
+      console.log(`EVENT::: `, event.keyCode);
+
+      // keyCodes between 65 and 90
+      if (event.keyCode >= 65 && event.keyCode <= 90) {
+        console.log(
+          `Letter ${event.key} with keycode ${event.keyCode} is valid pick.`
+        );
+        setGuessWord((guess) => guess.concat(event.key));
+      } else {
+        console.log(`This is invalid key.`);
+      }
+    }
+    document.addEventListener('keydown', keyPressHandler);
+    return () => document.removeEventListener('keydown', keyPressHandler);
+  }, []);
 
   useEffect(() => {
     if (effectRan.current === false) {
@@ -13,7 +32,7 @@ function App() {
         .then((response) => response.json())
         .then((data) => {
           const idx = index(data.length);
-          const pickedWord = data[idx].word;
+          const pickedWord = unifyString(data[idx].word, 'store');
           setWord(pickedWord);
         });
     }
@@ -22,7 +41,12 @@ function App() {
 
   return (
     <div className='flex flex-col items-center content-center'>
-      <h1 className='bg-slate-400 px-12 py-3 mt-10 text-rose-800 text-xl font-bold'>{word}</h1>
+      <h1 className='bg-slate-400 px-12 py-3 mt-10 text-rose-800 text-xl font-bold'>
+        {unifyString(word, 'display')}
+      </h1>
+      <h2 className='bg-slate-400 px-12 py-3 mt-10 text-rose-800 text-xl font-bold'>
+        |&gt;: {unifyString(guessWord, 'display')}
+      </h2>
 
       <Grid word={word} />
     </div>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,12 @@
 // takes a number as an argument
 // returns random integer between 0 and the given number
 export const randomIndex = (num) => Math.floor(Math.random() * num);
+
+export const unifyString = (string, criteria) => {
+  switch (criteria) {
+    case 'store':
+      return string.toLowerCase();
+    case 'display':
+      return string.toUpperCase();
+  }
+};
