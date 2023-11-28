67754bfb1dd04688acf8c1d2df9f3febeafc37b9
Ivan Skordev <maymunskoa@yahoo.com>
2023-11-29 01:05:36 +0200

    tiles are styled with corresponding colors

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,7 +4,14 @@ const Grid = ({ word, grid, guessWord, currentRow }) => {
   return (
     <div className='flex flex-col mt-6 gap-1'>
       {grid.map((line, i) => {
-        return <Row line={currentRow === i ? guessWord : line ?? ''} key={i} />;
+        return (
+          <Row
+            line={currentRow === i ? guessWord : line ?? ''}
+            key={i}
+            word={word}
+            visited={i < currentRow}
+          />
+        );
       })}
     </div>
   );
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,13 +1,21 @@
 import { unifyString } from '../utils/helpers';
-const Row = ({ line }) => {
+const Row = ({ line, word, visited }) => {
   const row = [];
   for (let i = 0; i < 5; i++) {
+    let className =
+      'flex border border-zinc-400 h-16 w-16 text-zinc-600 text-3xl items-center justify-center';
     let char = line[i];
+    if (visited) {
+      if (line[i] === word[i]) {
+        className += ' bg-wordle-green text-white';
+      } else if (word.includes(line[i])) {
+        className += ' bg-wordle-gold text-white';
+      } else {
+        className += ' bg-wordle-grey text-white';
+      }
+    }
     row.push(
-      <div
-        className='flex border border-zinc-400 h-16 w-16 text-zinc-600 text-3xl items-center justify-center'
-        key={i}
-      >
+      <div className={className} key={i}>
         {char ? unifyString(char, 'display') : char}
       </div>
     );
