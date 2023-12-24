69a35c744dfc4b4706182d6ab0fb6b90911a0127
Ivan Skordev <maymunskoa@yahoo.com>
2023-12-25 01:16:28 +0200

    hover effect on stack menu icon

--- src/components/icons/StackMenu.jsx
+++ src/components/icons/StackMenu.jsx
@@ -2,7 +2,7 @@ const StackMenu = ({ open, setOpenModal }) => {
   return (
     <div className='w-1/3'>
       <span
-        className='material-symbols-outlined text-3xl cursor-pointer'
+        className='material-symbols-outlined text-3xl cursor-pointer hover:text-stone-600'
         onClick={() =>
           setOpenModal((oldState) => ({ ...oldState, authModal: !open }))
         }
